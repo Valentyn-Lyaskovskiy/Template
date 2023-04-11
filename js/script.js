5af494c60a7c1d89d11618b61696df97272cd9cf
Valentyn Lyaskovskiy <riodemouro2017@gmail.com>
2023-04-11 15:50:54 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-const fixString = (string) => {
-  const temp = string.toLowerCase();
-  return temp[0].toUpperCase() + temp.slice(1)
-} 
+//            Fix string function
 
-console.log(fixString('vALEnTYN'));
-console.log(fixString('lYASkOVskiY'));
+// const fixString = (string) => {
+//   const temp = string.toLowerCase();
+//   return temp[0].toUpperCase() + temp.slice(1)
+// } 
+
+// console.log(fixString('vALEnTYN'));
+// console.log(fixString('lYASkOVskiY'));
+// console.log(fixString('ramBO'));
+
+//            Replace function
+
+const announcement = 'Mr Joe, come to the office! Mr Joe, asap!'
+console.log(announcement.replace('Joe', 'Mike'));
+console.log(announcement.replaceAll('Joe', 'Arnold'));
+console.log(announcement.replaceAll(/Mr/g, 'Doctor'));
