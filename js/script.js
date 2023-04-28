50a639577488c3603bf291e689dff188861ec630
Valentyn Lyaskovskiy <riodemouro2017@gmail.com>
2023-04-28 17:24:16 +0300

    ...

--- js/script.js
+++ js/script.js
@@ -26,3 +26,30 @@
 // const example = () => {
 //   let text // Using let - all stays here...function scope...
 //  }
+
+//          Touch     
+
+// window.addEventListener('DOMContentLoaded', () => {
+
+//   const box = document.querySelector('.box')
+
+//   box.addEventListener('touchstart', (e) => {
+//     e.preventDefault()
+//     console.log('Start!');
+//   })
+
+//   box.addEventListener('touchend', (e) => {
+//     e.preventDefault()
+//     console.log('End!');
+//   })
+// })
+
+const obj = {
+  a: 12
+}
+
+// console.log(obj);
+
+obj.a = 2023
+
+console.log(obj);
